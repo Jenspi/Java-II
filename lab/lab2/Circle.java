4254b8dd047f957baef26a6b36ef9692355ec7ec
Jennifer Spicer <xJenniferSpicer@gmail.com>
2020-01-21 12:14:01 -0600

    Testing javadocs

--- lab/lab2/Circle.java
+++ lab/lab2/Circle.java
@@ -9,31 +9,31 @@ public class Circle{
 	private double diameter, circ, area, radius;
 
 	/**
-	* This creates a circle object.
-	* @param radius This is the circle's radius.
+	* Constructs a circle object.
+	* @param radius the circle's radius
 	*/
 	public Circle(double radius){
 		this.radius = radius;
 	}
 
 	/**
-	* This method won't show up on our javadocs page because it's private.
+	* This method won't show up on this javadocs page because it's private.
 	*/
 	private void privateMethod(){
 		System.out.println("AAAAAAAA");
 	}
 
 	/**
-	* This allows us to set the circumference of our circle.
-	* @param circ The circumference of our circle.
+	* Sets new circumference for this circle.
+	* @param circ new circumference of this circle
 	*/
 	public void setCirc(double circ){
 		this.circ = circ;
 	}
 
 	/**
-	* This is how to get the circumference of our circle.
-	* @return The circumference of our cicle.
+	* Returns the circumference of this circle.
+	* @return the circumference of this cicle
 	*/
 	public double getCirc(){
 		return this.circ;
